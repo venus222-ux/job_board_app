a8dade59deaa497962c2d6e14250a14b0221fefe
STEFANIA IONESCU <stefanya_yo_18@yahoo.com>
2026-03-22 01:14:56 +0200

    Make some changes like delete the boostrap from main.tsx, add Hemet Provider to main.tx, delete proptyps pachakge

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,15 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "react-toastify/dist/ReactToastify.css";
-import { HelmetProvider } from "react-helmet-async"; // ✅ import HelmetProvider
+import { HelmetProvider } from "react-helmet-async";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <HelmetProvider>
-      {" "}
-      {/* ✅ wrap App with HelmetProvider */}
       <App />
     </HelmetProvider>
   </React.StrictMode>,
